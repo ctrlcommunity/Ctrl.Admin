c911e52ddaf6c6194e7787c4e8aa155fb5d639ea
辉 冯 <695979933@qq.com>
2019-09-16 22:19:42 +0800

    初次修改定义taghelper

---
+++ Core/Ctrl.Core.Tag/Controls/Button/CtrlButtonType.cs
@@ -0,0 +1,33 @@
+﻿namespace Ctrl.Core.Tag.Controls.Button
+{
+    /// <summary>
+    ///     按钮类型
+    /// </summary>
+    public enum CtrlButtonType
+    {
+        /// <summary>
+        ///  默认样式
+        /// </summary>
+        Default,
+        /// <summary>
+        ///     首选项
+        /// </summary>
+        Primary,
+        /// <summary>
+        ///      成功
+        /// </summary>
+        Success,
+        /// <summary>
+        ///  一般信息
+        /// </summary>
+        Info,
+        /// <summary>
+        ///  警告
+        /// </summary>
+        Warning,
+        /// <summary>
+        ///  危险
+        /// </summary>
+        Danger
+    }
+}
---
+++ Core/Ctrl.Core.Tag/CtrlTagHelperBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Ctrl.Core.Tag
+{
+    /// <summary>
+    ///     taghelper基类
+    /// </summary>
+    public class CtrlTagHelperBase:TagHelper
+    {
+
+    }
+}
