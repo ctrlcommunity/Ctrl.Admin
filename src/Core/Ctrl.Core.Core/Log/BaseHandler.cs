35cb968a36a689830c328c1d388680c9f149ac83
辉 冯 <695979933@qq.com>
2019-12-06 12:05:29 +0800

    修复 缺少Ctrl.Core.Core.Log 无法运行 #1

---
+++ src/Core/Ctrl.Core.Core/Log/BaseHandler.cs
@@ -0,0 +1,51 @@
+﻿using NLog;
+
+namespace Ctrl.Core.Core.Log
+{
+    /// <summary>
+    ///     说明：日志记录基类
+    /// </summary>
+    /// <typeparam name="TLog"></typeparam>
+    public abstract class BaseHandler<TLog>
+    {
+        #region 构造函数
+        /// <summary>
+        ///     构造函数初始化
+        /// </summary>
+        /// <param name="loggerConfig"></param>
+        protected BaseHandler(string loggerConfig) {
+            LoggerConfig = loggerConfig;
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        ///     写入日志，虚函数，可进行重写
+        /// </summary>
+        public virtual void WriteLog() {
+            if (string.IsNullOrEmpty(LoggerConfig))
+            {
+                return;
+            }
+            Logger iLog = LogManager.GetLogger(LoggerConfig);
+            if (iLog.IsInfoEnabled)
+            {
+            
+                iLog.Info(log);
+            }
+        }
+
+        #endregion
+
+
+
+        #region 属性
+        /// <summary>
+        ///     需要启动的日志模式名称
+        /// </summary>
+        private string LoggerConfig { get; set; }
+
+        public TLog log { get; set; }
+        #endregion
+    }
+}
---
+++ src/Core/Ctrl.Core.Core/Log/NLogLayout/PropertyLayoutAppend.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+using NLog.LayoutRenderers;
+using System.Reflection;
+using System.Text;
+
+namespace Ctrl.Core.Core.Log.NLogLayout
+{
+    [LayoutRenderer("Convert")]
+    public sealed class PropertyLayoutAppend : LayoutRenderer
+    {
+        /// <summary>
+        ///     属性值
+        /// </summary>
+        public string Property { get; set; }
+        /// <summary>
+        ///    重写基类转换器,根据配置的property key获取消息对象上的相应属性
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="logEvent"></param>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            if (Property != null)
+                builder.Append(LookupProperty(Property, logEvent));
+        }
+        /// <summary>
+        ///     通过反射获取传入的日志对象的某个属性的值
+        /// </summary>
+        /// <returns></returns>
+        private static object LookupProperty(string property,LogEventInfo logEvent) {
+            object messageObject = logEvent.Parameters[0];
+            PropertyInfo propertyInfo = messageObject.GetType().GetProperty(property);
+            object propertyValue = propertyInfo != null ? propertyInfo.GetValue(messageObject, null) : string.Empty;
+            return propertyValue;
+        }
+    }
+}
